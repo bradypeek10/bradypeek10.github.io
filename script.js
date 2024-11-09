f3a4c68e3199a34830a73dc750ed450052ed2ffc
bradypeek10 <70279452+bradypeek10@users.noreply.github.com>
2024-11-08 22:56:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,19 +4,8 @@ function showit(var1) {
   document.getElementById('first2').style.display = 'none';
   document.getElementById('first3').style.display = 'none';
   document.getElementById('first4').style.display = 'none';
-  //show the one div sent to the function
+  document.getElementById('first5').style.display = 'none';
   document.getElementById(var1).style.display = 'block';
 }
 var count = 1
 
-function select(input) {
-    if (count % 2 === 0) {
-      document.getElementById(input.id).className = "used1"
-      document.getElementById(input.id).textContent = "O"
-    }
-    else {
-      document.getElementById(input.id).className = "used2"
-      document.getElementById(input.id).textContent = "X"
-    }
-    count+=1
-  }
